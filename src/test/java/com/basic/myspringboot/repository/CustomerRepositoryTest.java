72b1e2e848c7ba9ddb14755c9d9a6cf2d7f335f1
Echae0 <edujcm01@gmail.com>
2025-05-07 16:46:55 +0900

    test update

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -1,23 +1,46 @@
 package com.basic.myspringboot.repository;
 
 import com.basic.myspringboot.entity.Customer;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+//@DataJpaTest
 class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
 
     @Test
+        //@Disabled
+    void testFindBy() {
+        Optional<Customer> optionalCustomer = customerRepository.findById(1L);
+        //assertThat(optionalCustomer).isNotEmpty();
+        if(optionalCustomer.isPresent()) {
+            Customer existCustomer = optionalCustomer.get();
+            assertThat(existCustomer.getId()).isEqualTo(1L);
+        }
+        //Optional 의 T orElseGet(Supplier<? extends T> supplier)
+        //Supplier 의 추상메서드 T get()
+        Optional<Customer> optionalCustomer2 = customerRepository.findByCustomerId("A001");
+        Customer a001customer = optionalCustomer2.orElseGet(() -> new Customer());
+        assertThat(a001customer.getCustomerName()).isEqualTo("스프링");
+
+    }
+
+    @Test
     @Rollback(value = false) //Rollback 처리하지 마세요!!
+    @Disabled
     void testCreateCustomer() {
         //Given (준비 단계)
         Customer customer = new Customer();
@@ -29,4 +52,6 @@ class CustomerRepositoryTest {
         assertThat(addCustomer).isNotNull();
         assertThat(addCustomer.getCustomerName()).isEqualTo("스프링2");
     }
+
+
 }
